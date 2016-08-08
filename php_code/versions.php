1abea1eedf2b0d501a3e3e179ea0a363a3adf1db
Marko Lindqvist <cazfi74@gmail.com>
2016-08-08 19:05:40 +0000

    Support for followtag specific comments to be send to freeciv clients.
    [[originally from svn r70]]

--- php_code/versions.php
+++ php_code/versions.php
@@ -4,5 +4,6 @@ $freeciv_versions = array('stable' => '2.3.1',
                           'S2_4' => '2.3.99-alpha',
                           'S2_5' => '2.4.99-dev');
 
-?>
+$version_comments = array();
 
+?>
