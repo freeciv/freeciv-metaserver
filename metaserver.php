ce4c732d7dbaa5f8f6e8fd5046a8b54a40dc2e20
Jacob Nevins <0jacobnk.fcdev@chiark.greenend.org.uk>
2017-05-06 22:29:17 +0000

    Add copyright statements.
    The set of copyright holders, and the statement that code is under
    GPLv2 or later, is based on (i) the licence statement and code history
    on the project page at gna.org; (ii) the following emails:
    
    From: Vasco Alexandre da Silva Costa <vasco.costa@gmail.com>
    Date: 22 July 2007 at 01:13
    Subject: Freeciv LAMP Metaserver + Client stats + Polls
    To: Freeciv developers <developers@freeciv.org>
    
    Hello folks,
    
    I dusted off the latest versions of these PHP scripts we have running
    at old.freeciv.org.
    I believe much of it was done by Paul and Reinier, the PHP Metaserver
    being a joint effort with more people, including Mike and me. I
    propose we use the same license as for Freeciv.
    
    I think these scripts provide critical functionality. Not only for
    users, but for developers. So we have more feedback on the needs and
    environments of our users.
    
    I guess this covers much of the remaining functionality, still at
    old.freeciv.org, we had not yet moved to outsourced services. The
    remaining interesting public old.freeciv.org services have been moved
    to MediaWiki at Wikia, the SVN service at Gna.org, or the file
    download service at SourceForge.net.
    
    I scrubbed for passwords, e-mails, db names. Please let me know if I
    forgot something, and grant permission so we can put this at Gna for
    safekeeping as the Freeciv Metaserver project.
    
    All the best,
    
    --
    Vasco Alexandre da Silva Costa
    
    From: Vasco Alexandre da Silva Costa <vasco.costa@gmail.com>
    Date: 30 July 2007 at 08:39
    Subject: Re: Freeciv LAMP Metaserver + Client stats + Polls
    To: Freeciv developers <developers@freeciv.org>
    
    [snip quoting]
    
    Given that Reinier and Paul are ok with their code being made public,
    the files were uploaded to the repository at:
    https://gna.org/projects/freecivmetaserve/
    
    [snip]
    
    [[originally from svn r73]]

--- metaserver.php
+++ metaserver.php
@@ -1,4 +1,11 @@
 <?php
+# Copyright (C) 2007 - Paul Zastoupil, Reinier Post, Mike Kaufman,
+#   Vasco Costa, Marko Lindqvist
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 
 /* do we want debug output to stderr?
  * This is very heavy so never leave it on in production
